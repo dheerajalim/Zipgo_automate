8962129df1e350be90f9ecca3cd06aaf0c9ef56c
dheerajalim <dheeraj.alim@gmail.com>
2017-04-17 17:39:20 +0530

    Added comments in zipgo_example.py

--- zipgo_example.py
+++ zipgo_example.py
@@ -9,8 +9,8 @@ class TestZipgo(unittest.TestCase):
 
 	@classmethod
 	def setUpClass(cls):
-
-		cls.driver = webdriver.Chrome('C:\Personal\Development\Python\seliniumtest\chromedriver.exe')
+		#You can alos use FireFox and IE, Xpath's may require change in that case
+		cls.driver = webdriver.Chrome('Place the path of Chrome Extension')
 		cls.driver.maximize_window()
 		cls.driver.implicitly_wait(10)
 
@@ -20,19 +20,23 @@ class TestZipgo(unittest.TestCase):
 	def test01_Routes(self):
 
 		driver = self.driver
-
+		#click on the navigation bar link Routes&Schedules
 		routes_link = driver.find_element_by_xpath('//*[@id="navbar"]/ul/li[2]/a')
 		routes_link.click()
 		time.sleep(2)
-
+		
+		#Finding the Fields for Origin and Destination
 		origin = driver.find_element_by_name("pickup_location_info[location_text]")
 		destination = driver.find_element_by_name("destination_location_info[location_text]")
 
 		origin.clear()
 		destination.clear()
-
+		
+		#Entering the data in the origin field
 		origin.send_keys("Kor")
 		time.sleep(2)
+		#Using ActionChains to select the value from the dropdown list
+		
 		#ActionChains(self.driver).key_down(Keys.DOWN).key_down(Keys.RETURN).perform()
 		ActionChains(self.driver).send_keys(Keys.DOWN,Keys.RETURN).perform()
 
@@ -43,6 +47,7 @@ class TestZipgo(unittest.TestCase):
 
 		time.sleep(5)
 
+		#Finding and Clicking the View Schedule button
 		view_schedule_button  = driver.find_element_by_class_name("view-trips-js")
 		view_schedule_button.click()
 
@@ -50,13 +55,14 @@ class TestZipgo(unittest.TestCase):
 
 		driver.find_element_by_class_name("close-trips-div").click()
 
-
+	#Testing the Send SMS field and the Success message
 	def test02_getapplink(self):
 
 		driver = self.driver
 
 		phone = driver.find_element_by_name("phone")
 		phone.clear()
+		#Replace 'Your Number' with the correct number
 		phone.send_keys("Your Number")
 		phone.submit()
 
